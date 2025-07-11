76be7d3833b6bfcacc04160df6ad4a0c6a6278bd
Arul Patmose Paramanathan <7695900+arulpatmose@users.noreply.github.com>
2025-07-12 02:30:28 +0530

    Added all requried models and refactored the filenames

---
+++ app/Models/CommercialModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CommercialModel extends Model
+{
+    protected $table            = 'commercials';
+    protected $primaryKey       = 'com_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['ucom_id', 'name', 'duration', 'format', 'client', 'added_by', 'category', 'sub_category', 'remarks', 'link'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
---
+++ app/Models/UserModel.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use CodeIgniter\Shield\Models\UserModel as ShieldUserModel;
+
+class UserModel extends ShieldUserModel
+{
+    protected function initialize(): void
+    {
+        parent::initialize();
+
+        $this->allowedFields = [
+            ...$this->allowedFields,
+            'first_name',
+            'last_name',
+            'deleted_at'
+        ];
+    }
+
+    public function getUsers($returnFields, $columnName, $columnSortOrder, $rowsPerPage, $start, $searchValue = "")
+    {
+        $data = array();
+
+        $builder = $this->db->table('users');
+        $builder->where('deleted_at IS NULL', null, false);
+
+        // Total number of records without filtering
+
+        $data['totalRecords'] = $builder->countAllResults();
+
+        // Total number of records with filtering
+
+        $query = $builder->select('users.id');
+
+        if ($searchValue != '') {
+            $query->orLike('first_name', $searchValue);
+        }
+
+        $builder->where('deleted_at IS NULL', null, false);
+
+        $data['totalRecordwithFilter'] = $builder->countAllResults(false);
+
+        // Fetch Records
+
+        $query = $builder->select($returnFields);
+
+        if ($searchValue != '') {
+            $query->orLike('first_name', $searchValue);
+        }
+
+        if (isset($columnName) &&  isset($columnSortOrder)) {
+            $query->orderBy($columnName, $columnSortOrder);
+        }
+
+        if (isset($rowsPerPage)) {
+            $query->limit($rowsPerPage);
+        }
+
+        if (isset($start)) {
+            $query->offset($start);
+        }
+
+        $data['records'] = $query->get()->getResultArray();
+
+        return ($data);
+    }
+
+    // Function to restore a deleted user
+    public function restoreUser($id)
+    {
+        return $this->set('deleted_at', null)
+            ->where('id', $id)
+            ->update();
+    }
+}
