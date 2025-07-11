f4bb37752d8229030b6b27fad244a866e378444e
Arul Patmose Paramanathan <7695900+arulpatmose@users.noreply.github.com>
2025-07-12 02:02:52 +0530

    Setup and configured codeigniter shield 4

--- app/Config/AuthGroups.php
+++ app/Config/AuthGroups.php
@@ -23,7 +23,7 @@ class AuthGroups extends ShieldAuthGroups
      * --------------------------------------------------------------------
      * The group that a newly registered user is added to.
      */
-    public string $defaultGroup = 'user';
+    public string $defaultGroup = 'scheduler';
 
     /**
      * --------------------------------------------------------------------
@@ -61,6 +61,18 @@ class AuthGroups extends ShieldAuthGroups
             'title'       => 'Beta User',
             'description' => 'Has access to beta-level features.',
         ],
+        'scheduler' => [
+            'title'       => 'Scheduling Officer',
+            'description' => 'Commercial Scheduling Officers',
+        ],
+        'webeditor' => [
+            'title'       => 'Content Creator',
+            'description' => 'Creates and manages digital contents',
+        ],
+        'marketingex' => [
+            'title'       => 'Marketing Executive',
+            'description' => 'Drive profit and promote products and services for ITN Digital',
+        ]
     ];
 
     /**
@@ -72,13 +84,49 @@ class AuthGroups extends ShieldAuthGroups
      * If a permission is not listed here it cannot be used.
      */
     public array $permissions = [
-        'admin.access'        => 'Can access the sites admin area',
-        'admin.settings'      => 'Can access the main site settings',
-        'users.manage-admins' => 'Can manage other admins',
-        'users.create'        => 'Can create new non-admin users',
-        'users.edit'          => 'Can edit existing non-admin users',
-        'users.delete'        => 'Can delete existing non-admin users',
-        'beta.access'         => 'Can access beta-level features',
+        'admin.access'          => 'Can access the sites admin area',
+        'admin.settings'        => 'Can access the main site settings',
+        'dashboard.access'      => 'Can access the dashboard',
+        'users.manage-admins'   => 'Can manage other admins',
+        'users.create'          => 'Can create new non-admin users',
+        'users.edit'            => 'Can edit existing non-admin users',
+        'users.delete'          => 'Can delete existing non-admin users',
+        'beta.access'           => 'Can access beta-level features',
+        'clients.access'        => 'Can access clients data',
+        'clients.create'        => 'Can create new client',
+        'clients.edit'          => 'Can edit existing client',
+        'clients.delete'        => 'Can delete existing client',
+        'commercials.access'    => 'Can access commercials data',
+        'commercials.create'    => 'Can create new commercial',
+        'commercials.edit'      => 'Can edit existing commercial',
+        'commercials.delete'    => 'Can delete existing commercial',
+        'formats.access'        => 'Can access formats data',
+        'formats.create'        => 'Can create new format',
+        'formats.edit'          => 'Can edit existing format',
+        'formats.delete'        => 'Can delete existing format',
+        'platforms.access'      => 'Can access platforms data',
+        'platforms.create'      => 'Can create new platform',
+        'platforms.edit'        => 'Can edit existing platform',
+        'platforms.delete'      => 'Can delete existing platform',
+        'spots.access'          => 'Can access spots data',
+        'spots.create'          => 'Can create new spot',
+        'spots.edit'            => 'Can edit existing spot',
+        'spots.delete'          => 'Can delete existing spot',
+        'programs.access'       => 'Can access programs data',
+        'programs.create'       => 'Can create new program',
+        'programs.edit'         => 'Can edit existing program',
+        'programs.delete'       => 'Can delete existing program',
+        'schedule.access'       => 'Can access schedule data',
+        'schedule.create'       => 'Can create new schedule',
+        'schedule.edit'         => 'Can edit existing schedule',
+        'schedule.delete'       => 'Can delete existing schedule',
+        'schedules.access'      => 'Can access schedules data',
+        'schedules.create'      => 'Can create new schedule',
+        'schedules.edit'        => 'Can edit existing schedule',
+        'schedules.delete'      => 'Can delete existing schedule',
+        'dailyschedule.access'  => 'Can access daily schedules data',
+        'dailyschedule.edit'    => 'Can edit existing daily schedules',
+        'accounts.access'       => 'Can access accounts data',
     ];
 
     /**
@@ -92,15 +140,31 @@ class AuthGroups extends ShieldAuthGroups
     public array $matrix = [
         'superadmin' => [
             'admin.*',
+            'dashboard.*',
             'users.*',
             'beta.*',
+            'clients.*',
+            'programs.*',
+            'commercials.*',
+            'formats.*',
+            'platforms.*',
+            'spots.*',
+            'schedule.*',
+            'schedules.*',
+            'dailyschedule.*',
+            'accounts.*'
         ],
         'admin' => [
             'admin.access',
+            'dashboard.*',
             'users.create',
             'users.edit',
             'users.delete',
             'beta.access',
+            'schedule.*',
+            'schedules.*',
+            'programs.*',
+            'dailyschedule.*'
         ],
         'developer' => [
             'admin.access',
@@ -109,9 +173,56 @@ class AuthGroups extends ShieldAuthGroups
             'users.edit',
             'beta.access',
         ],
+        'webeditor' => [
+            'dashboard.*',
+            'dailyschedule.*',
+            'clients.access',
+            'programs.access',
+            'commercials.access',
+            'formats.access',
+            'platforms.access',
+            'spots.access',
+            'schedule.access',
+            'schedule.create',
+            'schedule.edit',
+            'schedules.access',
+            'schedules.create',
+            'schedules.edit',
+        ],
         'user' => [],
         'beta' => [
             'beta.access',
         ],
+        'scheduler' => [
+            'dashboard.*',
+            'dailyschedule.access',
+            'clients.access',
+            'clients.create',
+            'clients.edit',
+            'programs.access',
+            'commercials.access',
+            'commercials.create',
+            'commercials.edit',
+            'formats.access',
+            'platforms.access',
+            'spots.access',
+            'schedule.access',
+            'schedule.create',
+            'schedule.edit',
+            'schedules.access',
+            'schedules.create',
+            'schedules.edit'
+        ],
+        'accountant' => [
+            'dashboard.*',
+            'accounts.*'
+        ],
+        'marketingex' => [
+            'dashboard.*',
+            'commercials.access',
+            'clients.access',
+            'schedules.access',
+            'schedule.access',
+        ]
     ];
 }
--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -88,5 +88,5 @@ class Autoload extends AutoloadConfig
      *
      * @var list<string>
      */
-    public $helpers = ['auth', 'setting'];
+    public $helpers = ['auth', 'setting', 'format_name', 'format_company_name', 'build_navigation'];
 }
