ce58e6f1afff04d86bc6a4456f097c6f54cb3920
Arul Patmose Paramanathan <7695900+arulpatmose@users.noreply.github.com>
2025-07-12 03:00:35 +0530

    Implemented DailySchedule controller methods aligning with Dashboard controller structure for consistency

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -9,6 +9,9 @@ class Dashboard extends BaseController
 {
     public function index()
     {
-        //
+        $data['page_title'] = "Dashboard";
+        $data['page_description'] = "Welcome to ITN Digital Portal! Explore a world of information, resources, and tools at your fingertips.";
+
+        return view('backend/dashboard', $data);
     }
 }
