17b16e97276c63c015c20292d6d7e29185f10b0f
Arul Patmose Paramanathan <7695900+arulpatmose@users.noreply.github.com>
2025-07-12 01:38:25 +0530

    Added CodeIgniter 4 Shield for enhanced authentication and authorization management

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'webmaster@itn.lk';
+    public string $fromName   = 'ITN Digital';
     public string $recipients = '';
 
     /**
