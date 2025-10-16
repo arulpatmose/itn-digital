23e4786d907f7a3ffa37d18e5be27bc0eac6b8ff
Arul Patmose Paramanathan <7695900+arulpatmose@users.noreply.github.com>
2025-10-16 20:49:10 +0530

    Add permission filters to user, client, schedule, program, spot, format, platform, commercial, and daily schedule routes for enhanced access control

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,18 +12,18 @@ $routes->get('/', 'Dashboard::index', ['as' => 'dashboard']);
 | User Management Routes
 | --------------------------------------------------------------------
 */
-$routes->group('users', function ($routes) {
-    $routes->get('/', 'Users::index', ['as' => 'users']);
-    $routes->get('add', 'Users::create');
-    $routes->post('submit', 'Users::store');
-    $routes->post('update-profile', 'Users::updateProfile');
-    $routes->post('update-user', 'Users::updateUser');
-    $routes->post('update-password', 'Users::changePassword');
-    $routes->post('update-user-password', 'Users::changeUserPassword');
-    $routes->post('update-user-groups', 'Users::updateUserGroups');
+$routes->group('users', ['filter' => 'permission:users.manage-admins'], function ($routes) {
+    $routes->get('/', 'Users::index', ['as' => 'users', 'filter' => 'permission:users.manage-admins']);
+    $routes->get('add', 'Users::create', ['filter' => 'permission:users.create']);
+    $routes->post('submit', 'Users::store', ['filter' => 'permission:users.create']);
+    $routes->post('update-profile', 'Users::updateProfile', ['filter' => 'permission:users.edit']);
+    $routes->post('update-user', 'Users::updateUser', ['filter' => 'permission:users.edit']);
+    $routes->post('update-password', 'Users::changePassword', ['filter' => 'permission:users.edit']);
+    $routes->post('update-user-password', 'Users::changeUserPassword', ['filter' => 'permission:users.edit']);
+    $routes->post('update-user-groups', 'Users::updateUserGroups', ['filter' => 'permission:users.manage-admins']);
     $routes->get('profile', 'Users::profile');
-    $routes->get('edit/(:num)', 'Users::edit/$1');
-    $routes->post('delete', 'Users::deleteUser');
+    $routes->get('edit/(:num)', 'Users::edit/$1', ['filter' => 'permission:users.edit']);
+    $routes->post('delete', 'Users::deleteUser', ['filter' => 'permission:users.delete']);
 });
 
 /*
@@ -31,116 +31,121 @@ $routes->group('users', function ($routes) {
 | Client Management Routes
 | --------------------------------------------------------------------
 */
-$routes->group('clients', function ($routes) {
+$routes->group('clients', ['filter' => 'permission:clients.access'], function ($routes) {
     $routes->get('/', 'Clients::index', ['as' => 'clients']);
-    $routes->get('add', 'Clients::create');
-    $routes->post('submit', 'Clients::store');
-    $routes->get('edit/(:num)', 'Clients::edit/$1');
-    $routes->post('update/(:num)', 'Clients::update/$1');
-    $routes->post('delete', 'Clients::destroy');
+    $routes->get('add', 'Clients::create', ['filter' => 'permission:clients.create']);
+    $routes->post('submit', 'Clients::store', ['filter' => 'permission:clients.create']);
+    $routes->get('edit/(:num)', 'Clients::edit/$1', ['filter' => 'permission:clients.edit']);
+    $routes->post('update/(:num)', 'Clients::update/$1', ['filter' => 'permission:clients.edit']);
+    $routes->post('delete', 'Clients::destroy', ['filter' => 'permission:clients.delete']);
 });
 
 /*
 | --------------------------------------------------------------------
-| Schedule Routes (Bulk & Individual)
+| Schedule Routes (Bulk)
 | --------------------------------------------------------------------
 */
-$routes->group('schedules', function ($routes) {
+$routes->group('schedules', ['filter' => 'permission:schedules.access'], function ($routes) {
     $routes->get('/', 'Schedules::index', ['as' => 'schedules']);
-    $routes->get('add', 'Schedules::create');
-    $routes->post('submit', 'Schedules::store');
-    $routes->get('edit/(:num)', 'Schedules::edit/$1');
-    $routes->post('update/(:num)', 'Schedules::update/$1');
-    $routes->post('delete', 'Schedules::destroy');
-    $routes->post('fetch-comments', 'Schedules::fetchComments');
+    $routes->get('add', 'Schedules::create', ['filter' => 'permission:schedules.create']);
+    $routes->post('submit', 'Schedules::store', ['filter' => 'permission:schedules.create']);
+    $routes->get('edit/(:num)', 'Schedules::edit/$1', ['filter' => 'permission:schedules.edit']);
+    $routes->post('update/(:num)', 'Schedules::update/$1', ['filter' => 'permission:schedules.edit']);
+    $routes->post('delete', 'Schedules::destroy', ['filter' => 'permission:schedules.delete']);
+    $routes->post('fetch-comments', 'Schedules::fetchComments', ['filter' => 'permission:schedules.access']);
 });
 
-$routes->group('schedule', function ($routes) {
+/*
+| --------------------------------------------------------------------
+| Individual Schedule Routes
+| --------------------------------------------------------------------
+*/
+$routes->group('schedule', ['filter' => 'permission:schedule.access'], function ($routes) {
     $routes->get('(:num)', 'Schedule::index/$1');
-    $routes->get('add/(:num)', 'Schedule::create/$1');
-    $routes->post('submit/(:num)', 'Schedule::store/$1');
-    $routes->post('delete', 'Schedule::destroy');
+    $routes->get('add/(:num)', 'Schedule::create/$1', ['filter' => 'permission:schedule.create']);
+    $routes->post('submit/(:num)', 'Schedule::store/$1', ['filter' => 'permission:schedule.create']);
+    $routes->post('delete', 'Schedule::destroy', ['filter' => 'permission:schedule.delete']);
 });
 
 /*
 | --------------------------------------------------------------------
-| Program Routes
+| Programs
 | --------------------------------------------------------------------
 */
-$routes->group('programs', function ($routes) {
+$routes->group('programs', ['filter' => 'permission:programs.access'], function ($routes) {
     $routes->get('/', 'Programs::index', ['as' => 'programs']);
-    $routes->get('add', 'Programs::create');
-    $routes->post('submit', 'Programs::store');
-    $routes->get('edit/(:num)', 'Programs::edit/$1');
-    $routes->post('update/(:num)', 'Programs::update/$1');
-    $routes->post('delete', 'Programs::destroy');
+    $routes->get('add', 'Programs::create', ['filter' => 'permission:programs.create']);
+    $routes->post('submit', 'Programs::store', ['filter' => 'permission:programs.create']);
+    $routes->get('edit/(:num)', 'Programs::edit/$1', ['filter' => 'permission:programs.edit']);
+    $routes->post('update/(:num)', 'Programs::update/$1', ['filter' => 'permission:programs.edit']);
+    $routes->post('delete', 'Programs::destroy', ['filter' => 'permission:programs.delete']);
 });
 
 /*
 | --------------------------------------------------------------------
-| Spot Routes
+| Spots
 | --------------------------------------------------------------------
 */
-$routes->group('spots', function ($routes) {
+$routes->group('spots', ['filter' => 'permission:spots.access'], function ($routes) {
     $routes->get('/', 'Spots::index', ['as' => 'spots']);
-    $routes->get('add', 'Spots::create');
-    $routes->post('submit', 'Spots::store');
-    $routes->get('edit/(:num)', 'Spots::edit/$1');
-    $routes->post('update/(:num)', 'Spots::update/$1');
-    $routes->post('delete', 'Spots::destroy');
+    $routes->get('add', 'Spots::create', ['filter' => 'permission:spots.create']);
+    $routes->post('submit', 'Spots::store', ['filter' => 'permission:spots.create']);
+    $routes->get('edit/(:num)', 'Spots::edit/$1', ['filter' => 'permission:spots.edit']);
+    $routes->post('update/(:num)', 'Spots::update/$1', ['filter' => 'permission:spots.edit']);
+    $routes->post('delete', 'Spots::destroy', ['filter' => 'permission:spots.delete']);
 });
 
 /*
 | --------------------------------------------------------------------
-| Format Routes
+| Formats
 | --------------------------------------------------------------------
 */
-$routes->group('formats', function ($routes) {
+$routes->group('formats', ['filter' => 'permission:formats.access'], function ($routes) {
     $routes->get('/', 'Formats::index', ['as' => 'formats']);
-    $routes->get('add', 'Formats::create');
-    $routes->post('submit', 'Formats::store');
-    $routes->get('edit/(:num)', 'Formats::edit/$1');
-    $routes->post('update/(:num)', 'Formats::update/$1');
-    $routes->post('delete', 'Formats::destroy');
+    $routes->get('add', 'Formats::create', ['filter' => 'permission:formats.create']);
+    $routes->post('submit', 'Formats::store', ['filter' => 'permission:formats.create']);
+    $routes->get('edit/(:num)', 'Formats::edit/$1', ['filter' => 'permission:formats.edit']);
+    $routes->post('update/(:num)', 'Formats::update/$1', ['filter' => 'permission:formats.edit']);
+    $routes->post('delete', 'Formats::destroy', ['filter' => 'permission:formats.delete']);
 });
 
 /*
 | --------------------------------------------------------------------
-| Platform Routes
+| Platforms
 | --------------------------------------------------------------------
 */
-$routes->group('platforms', function ($routes) {
+$routes->group('platforms', ['filter' => 'permission:platforms.access'], function ($routes) {
     $routes->get('/', 'Platforms::index', ['as' => 'platforms']);
-    $routes->get('add', 'Platforms::create');
-    $routes->post('submit', 'Platforms::store');
-    $routes->get('edit/(:num)', 'Platforms::edit/$1');
-    $routes->post('update/(:num)', 'Platforms::update/$1');
-    $routes->post('delete', 'Platforms::destroy');
+    $routes->get('add', 'Platforms::create', ['filter' => 'permission:platforms.create']);
+    $routes->post('submit', 'Platforms::store', ['filter' => 'permission:platforms.create']);
+    $routes->get('edit/(:num)', 'Platforms::edit/$1', ['filter' => 'permission:platforms.edit']);
+    $routes->post('update/(:num)', 'Platforms::update/$1', ['filter' => 'permission:platforms.edit']);
+    $routes->post('delete', 'Platforms::destroy', ['filter' => 'permission:platforms.delete']);
 });
 
 /*
 | --------------------------------------------------------------------
-| Commercial (Ads) Routes
+| Commercials
 | --------------------------------------------------------------------
 */
-$routes->group('commercials', function ($routes) {
+$routes->group('commercials', ['filter' => 'permission:commercials.access'], function ($routes) {
     $routes->get('/', 'Commercials::index', ['as' => 'commercials']);
-    $routes->get('add', 'Commercials::create');
-    $routes->post('submit', 'Commercials::store');
-    $routes->get('edit/(:num)', 'Commercials::edit/$1');
-    $routes->post('update/(:num)', 'Commercials::update/$1');
-    $routes->post('delete', 'Commercials::destroy');
+    $routes->get('add', 'Commercials::create', ['filter' => 'permission:commercials.create']);
+    $routes->post('submit', 'Commercials::store', ['filter' => 'permission:commercials.create']);
+    $routes->get('edit/(:num)', 'Commercials::edit/$1', ['filter' => 'permission:commercials.edit']);
+    $routes->post('update/(:num)', 'Commercials::update/$1', ['filter' => 'permission:commercials.edit']);
+    $routes->post('delete', 'Commercials::destroy', ['filter' => 'permission:commercials.delete']);
 });
 
 /*
 | --------------------------------------------------------------------
-| Daily Schedule Routes
+| Daily Schedule
 | --------------------------------------------------------------------
 */
-$routes->group('daily-schedule', function ($routes) {
-    $routes->post('edit/(:num)', 'DailySchedule::edit/$1');
-    $routes->post('update/(:num)', 'DailySchedule::update/$1');
-    $routes->post('update-bulk', 'DailySchedule::updateBulk');
+$routes->group('daily-schedule', ['filter' => 'permission:dailyschedule.access'], function ($routes) {
+    $routes->post('edit/(:num)', 'DailySchedule::edit/$1', ['filter' => 'permission:dailyschedule.edit']);
+    $routes->post('update/(:num)', 'DailySchedule::update/$1', ['filter' => 'permission:dailyschedule.edit']);
+    $routes->post('update-bulk', 'DailySchedule::updateBulk', ['filter' => 'permission:dailyschedule.edit']);
     $routes->post('fetch-comments', 'DailySchedule::fetchComments');
     $routes->get('/', 'DailySchedule::index/');
     $routes->get('(:segment)', 'DailySchedule::index/$1');
